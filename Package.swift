5d2293ff0d84b110a4a2dff6d8599d40ab74bc87
Armino Popp <devel@boioiong.com>
2020-05-29 01:00:48 +0300

    regenerating Package.swift

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+// Created by Armino <devel@boioiong.com>
+// GitHub: https://github.com/armino-dev/appwrite-swift-sdk
+
+
+import PackageDescription
+
+let package = Package(
+    name: "appwrite-swift-sdk",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "appwrite-swift-sdk",
+            targets: ["appwrite-swift-sdk"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "appwrite-swift-sdk",
+            dependencies: []),
+        .testTarget(
+            name: "appwrite-swift-sdkTests",
+            dependencies: ["appwrite-swift-sdk"]),
+    ]
+)
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import appwrite_swift_sdkTests
+
+var tests = [XCTestCaseEntry]()
+tests += appwrite_swift_sdkTests.allTests()
+XCTMain(tests)
